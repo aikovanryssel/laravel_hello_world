a3e41f1c03eee273b307fbdda9e36a24ba10257d
aiko.van.ryssel@student.howest.be <aiko.van.ryssel@student.howest.be>
2021-07-07 18:29:07 +0200

    update with sql database

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -1,51 +1,11 @@
 <?php
 
 namespace App\Models;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
-use Illuminate\Support\Facades\File;
-use Spatie\YamlFrontMatter\YamlFrontMatter;
 
-class Post
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
 {
-    public function __construct(public $title, $excerpt,$date,$body,$slug)
-    {
-        $this->title=$title;
-        $this->excerpt=$excerpt;
-        $this->date=$date;
-        $this->body=$body;
-        $this->slug=$slug;
-    }
-    public static function all(){
-        return cache()-> rememberForever('posts.all', function(){
-
-            return collect(File::files(resource_path("posts")))
-            ->map(function ($file){
-                return YamlFrontMatter::parseFile($file);
-            })
-            ->map(function ($document){
-                return new Post(
-                    $document->title,
-                    $document->excerpt,
-                    $document->date,
-                    $document->body(),
-                    $document->slug,
-                );
-            }
-        )->sortByDesc('date');
-        });
-    }
-    public static function find($slug){
-        $post= static::all()->firstWhere('slug',$slug);
-        if(!$post){
-            throw new ModelNotFoundException();
-        }
-        return $post;
-    //     if(! file_exists($path=resource_path("posts/{$slug}.html"))){
-    //         throw new ModelNotFoundException();
-    //     }
-    //   return cache()->remember("post.{$slug}", 1200, fn()=> file_get_contents($path));
-
-  
-    }
-
+    use HasFactory;
 }
