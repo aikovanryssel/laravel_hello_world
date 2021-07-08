94958d6b95652967aea77619e91f89b88141540c
aiko.van.ryssel@student.howest.be <aiko.van.ryssel@student.howest.be>
2021-07-08 17:13:36 +0200

    update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +16,55 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::truncate();
+        Category::truncate();
+        Post::truncate();
+
+        $user=User::factory()->create([
+            'name'=> 'Aiko'
+        ]);
+        Post::factory(5)->create([
+            'user_id'=>$user->id
+        ]);
+        // $personal =Category::create([
+        //     "name"=>'Personal',
+        //     'slug'=> 'personal'
+        // ]);
+        // $family =Category::create([
+        //     "name"=>'Family',
+        //     'slug'=> 'family'
+        // ]);
+        // $work =Category::create([
+        //     "name"=>'Work',
+        //     'slug'=> 'work'
+        // ]);
+
+        // Post::create([
+        //     'user_id'=>$user->id,
+        //     'category_id'=>$family->id,
+        //     'title'=> 'My Family post',
+        //     'slug'=> 'my-family-post',
+        //     'excerpt'=> 'Lorem Ipsum is slechts een proeftekst uit het drukkerij',
+        //     'body'=> 'In tegenstelling tot wat algemeen aangenomen wordt is Lorem Ipsum niet zomaar willekeurige tekst. het heeft zijn wortels in een stuk klassieke latijnse literatuur uit 45 v.Chr. en is dus meer dan 2000 jaar oud. Richard McClintock, een professor latijn aan de Hampden-Sydney College in Virginia, heeft één van de meer obscure latijnse woorden, consectetur, uit een Lorem Ipsum passage opgezocht, en heeft tijdens het zoeken naar het woord in de klassieke literatuur de onverdachte bron ontdekt. Lorem Ipsum komt uit de secties 1.10.32 en 1.10.33 van "de Finibus Bonorum et Malorum" (De uitersten van goed en kwaad) door Cicero, geschreven in 45 v.Chr. Dit boek is een verhandeling over de theorie der ethiek, erg populair tijdens de renaissance. De eerste regel van Lorem Ipsum, "Lorem ipsum dolor sit amet..", komt uit een zin in sectie 1.10.32.',
+
+        // ]);
+        // Post::create([
+        //     'user_id'=>$user->id,
+        //     'category_id'=>$personal->id,
+        //     'title'=> 'My Personal post',
+        //     'slug'=> 'my-personal-post',
+        //     'excerpt'=> 'Lorem Ipsum is slechts een proeftekst uit het drukkerij',
+        //     'body'=> 'In tegenstelling tot wat algemeen aangenomen wordt is Lorem Ipsum niet zomaar willekeurige tekst. het heeft zijn wortels in een stuk klassieke latijnse literatuur uit 45 v.Chr. en is dus meer dan 2000 jaar oud. Richard McClintock, een professor latijn aan de Hampden-Sydney College in Virginia, heeft één van de meer obscure latijnse woorden, consectetur, uit een Lorem Ipsum passage opgezocht, en heeft tijdens het zoeken naar het woord in de klassieke literatuur de onverdachte bron ontdekt. Lorem Ipsum komt uit de secties 1.10.32 en 1.10.33 van "de Finibus Bonorum et Malorum" (De uitersten van goed en kwaad) door Cicero, geschreven in 45 v.Chr. Dit boek is een verhandeling over de theorie der ethiek, erg populair tijdens de renaissance. De eerste regel van Lorem Ipsum, "Lorem ipsum dolor sit amet..", komt uit een zin in sectie 1.10.32.',
+
+        // ]);
+        // Post::create([
+        //     'user_id'=>$user->id,
+        //     'category_id'=>$work->id,
+        //     'title'=> 'My Work post',
+        //     'slug'=> 'my-work-post',
+        //     'excerpt'=> 'Lorem Ipsum is slechts een proeftekst uit het drukkerij',
+        //     'body'=> 'In tegenstelling tot wat algemeen aangenomen wordt is Lorem Ipsum niet zomaar willekeurige tekst. het heeft zijn wortels in een stuk klassieke latijnse literatuur uit 45 v.Chr. en is dus meer dan 2000 jaar oud. Richard McClintock, een professor latijn aan de Hampden-Sydney College in Virginia, heeft één van de meer obscure latijnse woorden, consectetur, uit een Lorem Ipsum passage opgezocht, en heeft tijdens het zoeken naar het woord in de klassieke literatuur de onverdachte bron ontdekt. Lorem Ipsum komt uit de secties 1.10.32 en 1.10.33 van "de Finibus Bonorum et Malorum" (De uitersten van goed en kwaad) door Cicero, geschreven in 45 v.Chr. Dit boek is een verhandeling over de theorie der ethiek, erg populair tijdens de renaissance. De eerste regel van Lorem Ipsum, "Lorem ipsum dolor sit amet..", komt uit een zin in sectie 1.10.32.',
+
+        // ]);
     }
 }
